0465e086deb8c07f0a57eadeb44b8e98b8326e8c
Tien Nam Dao <tien.dao2@tiki.vn>
2022-05-12 18:45:26 +0700

    chore: change package name

---
+++ packages/astrajs/src/index.ts
@@ -0,0 +1,7 @@
+import * as addressConverter from '@astraprotocol/address-converter'
+import * as eip712 from '@astraprotocol/eip712'
+import * as proto from '@astraprotocol/proto'
+import * as provider from '@astraprotocol/provider'
+import * as transactions from '@astraprotocol/transactions'
+
+export { addressConverter, eip712, proto, provider, transactions }
