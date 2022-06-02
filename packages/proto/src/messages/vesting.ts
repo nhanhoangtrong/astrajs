af4c87d670e216e8a65bf631ca21e3d94bdff57a
Tien Nam Dao <tien.dao2@tiki.vn>
2022-06-02 14:00:56 +0700

    fix: proto vesting

--- packages/proto/src/messages/vesting.ts
+++ packages/proto/src/messages/vesting.ts
@@ -1,7 +1,8 @@
-import { Timestamp } from 'google-protobuf/google/protobuf/timestamp_pb'
+/* eslint-disable camelcase */
 import * as vesting from '../proto/evmos/vesting/v1/tx'
 import * as coin from '../proto/cosmos/base/v1beta1/coin'
-import * as cosmosVesting from '../proto/cosmos/vesting/v1beta1/vesting'
+import * as cosmos_vesting from '../proto/cosmos/vesting/v1beta1/vesting'
+import * as googleTs from '../proto/google/protobuf/timestamp'
 
 export function createMsgVesting(
   fromAddress: string,
@@ -18,29 +19,32 @@ export function createMsgVesting(
   }[],
   merge: boolean = false,
 ) {
-  const vestingPeriodsData = vestingPeriods.map(
+  const vesting_periods = vestingPeriods.map(
     (v) =>
-      new cosmosVesting.cosmos.vesting.v1beta1.Period({
+      new cosmos_vesting.cosmos.vesting.v1beta1.Period({
         length: parseInt(v.length, 10),
         amount: [
           new coin.cosmos.base.v1beta1.Coin({
             denom: v.amount[0].denom,
-            amount: v.amount[0].amount,
+            amount: v.amount[0].amount.toString(),
           }),
         ],
       }),
   )
 
-  const message = new vesting.evmos.vesting.v1.MsgCreateClawbackVestingAccount([
-    {
-      from_address: fromAddress,
-      to_address: toAddress,
-      start_time: Timestamp.fromDate(new Date(startTime)),
-      // lockupPeriods: undefined,
-      vesting_periods: vestingPeriodsData,
-      merge,
-    },
-  ])
+  const time = new Date(startTime).getTime()
+  const tp = new googleTs.google.protobuf.Timestamp({
+    nanos: (time % 1000) * 1e6,
+    seconds: Math.round(time / 1000),
+  })
+
+  const message = new vesting.evmos.vesting.v1.MsgCreateClawbackVestingAccount({
+    from_address: fromAddress,
+    to_address: toAddress,
+    start_time: tp,
+    vesting_periods,
+    merge,
+  })
   return {
     message,
     path: 'evmos.vesting.v1.MsgCreateClawbackVestingAccount',
